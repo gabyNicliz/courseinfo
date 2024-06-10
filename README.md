3dd84af4b4d2174da354efddb29360e2e0b51e93
gabyNicliz <gabynicliz93@gmail.com>
2024-06-10 00:06:02 -0300

    exercises 1.3 to 1.5 completed

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Aplication will be developed through the course
 
 ## 1.2: Course Information, step 2
 * Refactor Content component to make it more readable
+
+## 1.3: Course Information step 3
+* Refactor code and create partX objects containing the course's part name and exercise count
+
+## 1.4: Course Information step 4
+* Place the part objects into a single array
+
+## 1.5: Course Information step 5
--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ const Part = (props) => {
 };
 
 const Header = (props) => {
+  console.log(props);
+
   return (
     <div>
       <h1>{props.course}</h1>
@@ -35,19 +37,23 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    { partName: 'Fundamentals of React', exercisesCount: 10},
-    { partName: 'Using props to pass data', exercisesCount: 7},
-    { partName: 'State of a component', exercisesCount: 14}
-  ];
+  const course = { 
+    name: 'Half Stack application development',
+    parts: [
+      { partName: 'Fundamentals of React', exercisesCount: 10},
+      { partName: 'Using props to pass data', exercisesCount: 7},
+      { partName: 'State of a component', exercisesCount: 14}
+    ],
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
       <Total 
-        total={parts[0].exercisesCount + parts[1].exercisesCount + parts[2].exercisesCount}
+        total={
+          course.parts[0].exercisesCount + course.parts[1].exercisesCount + course.parts[2].exercisesCount
+        }
       />
     </div>
   );
